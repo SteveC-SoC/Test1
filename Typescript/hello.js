837d89a14ccb2779732b50bdbac5fc4f957a872d
SteveC-SoC <s.p.cadden@gmail.com>
2021-07-30 15:50:44 +0100

    playing about with tests

---
+++ Typescript/hello.js
@@ -0,0 +1,7 @@
+// greet the world
+console.log("Hello World!");
+// This is an industrial-grade general-purpose greeter function:
+function greet(person, date) {
+    console.log("Hello " + person + ", today is " + date + "!");
+}
+greet("Brendan", 30 / 7 / 21);
---
+++ Typescript/hello.ts
@@ -0,0 +1,9 @@
+// greet the world
+console.log("Hello World!");
+
+// This is an industrial-grade general-purpose greeter function:
+function greet(person: string, date: Date) {
+    console.log(`Hello ${person}, today is ${date.toDateString}!`);
+  }
+  
+  greet("Brendan", new Date());
